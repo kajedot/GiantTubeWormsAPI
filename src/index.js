c15f466c19f3c011076c1c4d07bd7ddf57cbfa79
Kajetan D-T <kajedot@gmail.com>
2018-12-01 00:48:12 +0100

    new index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+
 const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
-});
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log('Listening on ' + port)) 
