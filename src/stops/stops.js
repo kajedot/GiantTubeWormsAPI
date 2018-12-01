cf86f5a00b343bf9dc3aa933bfaa098d4363866e
Marcin Wawrzków <marcin.wawrzkow@outlook.com>
2018-12-01 11:27:12 +0100

    6

--- src/stops/stops.js
+++ src/stops/stops.js
@@ -1,50 +1,6 @@
-var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('data/stops.txt')
-  });
+  var fs = require('fs');
 
-  var stopslist = [];
-  
-  
-  
- lineReader.on('line', function (line) {
-    var place = 0, ID = '',name = '',lon='',lat='', stopcode= '';
-    for(var i = 0; i != line.length; i++)
-    {
-        if(line.charAt(i) == ',')
-        {
-            place++;
-        }else
-        {
-            switch(place)
-            {
-                case 0:
-                ID += line.charAt(i);
-                break;
-                case 1:
-                stopcode+=line.charAt(i);
-                break;
-                case 2:
-                name+=line.charAt(i);
-                break;
-                case 3:
-                lon+=line.charAt(i);
-                break;
-                case 4:
-                lat+= line.charAt(i);
-                break;
-                default:
-            }
-            
-        }
-        
-    }
-    console.log(new stopinformation(ID,name.lon,lat,stopcode,));
-    stopslist.push(new stopinformation(ID,name,lon,lat,stopcode));
- 
-    
-  });
-  
-class stopinformation{
+  class stopinformation{
     constructor(ID,name,lon,lat,stopcode){
         this.lon = lon;
         this.lat=lat;
@@ -55,6 +11,52 @@ class stopinformation{
 }
 
 
- console.log(stopslist);
 
+function value()
+{
+    var file = fs.readFileSync('data/stops.txt', (err ,data)=>
+    {
+        console.log(err);
+    });
+    console.log(file);
+    
+    /*   
+    lineReader.on('line', function (line) {
+       var place = 0, ID = '',name = '',lon='',lat='', stopcode= '';
+       for(var i = 0; i != line.length; i++)
+       {
+           if(line.charAt(i) == ',')
+           {
+               place++;
+           }else
+           {
+               switch(place)
+               {
+                   case 0:
+                   ID += line.charAt(i);
+                   break;
+                   case 1:
+                   stopcode+=line.charAt(i);
+                   break;
+                   case 2:
+                   name+=line.charAt(i);
+                   break;
+                   case 3:
+                   lon+=line.charAt(i);
+                   break;
+                   case 4:
+                   lat+= line.charAt(i);
+                   break;
+                   default:
+               }
+               
+           }
+           
+       }
+       
+       set(new stopinformation(ID,name,lon,lat,stopcode));
+       
+     });*/
+}
 
+value();
