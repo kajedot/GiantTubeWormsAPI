5c0569899e9eec910d546d8bac3c79068011d769
Kajetan D-T <kajedot@gmail.com>
2018-12-01 05:10:14 +0100

    add post

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const app = express()
 
 const port=process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage')
+})
 
 app.get('/xd',(req,res) => {
 
