a1b7421550dd0acf4bf5fae8ce9cb81630427f94
Kajetan D-T <kajedot@gmail.com>
2018-12-01 01:03:58 +0100

    xd

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const port=process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/xd' (req, res) => res.send('xDxDxD'))
+app.get('/xd', (req, res) => res.send('xDxDxD'))
 
 app.listen(port, () => console.log('Listening on ' + port)) 
